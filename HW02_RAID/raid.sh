03674e6310b77603a8ff18938ebf141923c9f886
Conflictt <75241603+Conflictt@users.noreply.github.com>
2021-03-25 14:19:48 +0200

    1

--- HW02_RAID/raid.sh
+++ HW02_RAID/raid.sh
@@ -1,48 +1,36 @@
 #!/bin/bash
 
+#Занулить суперблоки
 mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
 
+#Создать рейд 5 из 5 дисков
 mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f}
 
-cat /proc/mdstat
-
-mdadm -D /dev/md0
-
-mdadm --detail --scan --verbose
-
+#Создать папку для файла конфигурации mdadm
 mkdir /etc/mdadm
 
+#Добавление информации о массиве в конфигурационный файл
 echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
-
 mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
 
-mdadm /dev/md0 --fail /dev/sde
-
-cat /proc/mdstat
-
-mdadm -D /dev/md0
-
-mdadm /dev/md0 --remove /dev/sde
-
-mdadm /dev/md0 --add /dev/sde
-
-cat /proc/mdstat
-
-mdadm -D /dev/md0
-
+#Создать gpt раздел на рейд массиве
 parted -s /dev/md0 mklabel gpt
 
+#Создать разделы
 parted /dev/md0 mkpart primary ext4 0% 20%
 parted /dev/md0 mkpart primary ext4 20% 40%
 parted /dev/md0 mkpart primary ext4 40% 60%
 parted /dev/md0 mkpart primary ext4 60% 80%
 parted /dev/md0 mkpart primary ext4 80% 100%
 
+#Создать файловые системы на разделах
 for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
 
+#Создание каталогов для монтирования
 mkdir -p /raid/part{1,2,3,4,5}
 
+#Монитрование разделов
 for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
 
-echo "#NEW DEVICE" >> /etc/fstab
-for i in $(seq 1 5); do echo `sudo blkid /dev/md0p$i | awk '{print $2}'` /u0$i ext4 defaults 0 0 >> /etc/fstab; done
+#Добавление информации о устройствах в fstab
+cat /etc/mtab | grep raid >> /etc/fstab
