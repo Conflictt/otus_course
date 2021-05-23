eac5286dc8e8eb0bee3ad658cc989139f3f26471
Conflictt <75241603+Conflictt@users.noreply.github.com>
2021-05-23 23:11:43 +0300

    update

--- HW06_RPM/rpm.sh
+++ HW06_RPM/rpm.sh
@@ -1,75 +1,46 @@
-#!/bin/bash
-
-# Для данного заданиā нам понадобятся следующие установленные пакеты:
-yum install -y \
-redhat-lsb-core \
-wget \
-rpmdevtools \
-rpm-build \
-createrepo \
-yum-utils
-
+#!/usr/bin/env bash
 # Загрузим SRPM пакет NGINX для дальнейшей работы над ним:
-wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm
-
+sudo -i
+wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.20.0-1.el7.ngx.src.rpm
 # При установке такого пакета в домашней директории создается древо каталогов для сборки:
-rpm -i nginx-1.14.1-1.el7_4.ngx.src.rpm
-
+rpm -i nginx-1.20.0-1.el7.ngx.src.rpm
 # Качаем исходники для openssl
 wget https://www.openssl.org/source/latest.tar.gz
-
 # Распаковываем их
 tar -xvf latest.tar.gz
-
 # Устанавливаем зависимости
-yum-builddep rpmbuild/SPECS/nginx.spec
-
-# Добавить в %build --with-openssl=/root/openssl-1.1.1a
-nano nano SPECS/nginx.spec
-
+yum-builddep -y rpmbuild/SPECS/nginx.spec
+# Замена SPEC файла
+cp -f /vagrant/nginx.spec /root/rpmbuild/SPECS/nginx.speс
 # Собираем пакет
-rpmbuild -bb rpmbuild/SPECS/nginx.spec
-
+rpmbuild -bb /root/rpmbuild/SPECS/nginx.speс
 # Убеждаемся что пакет создан
-ll rpmbuild/RPMS/x86_64/
-
+ll /root/rpmbuild/RPMS/x86_64/
 # Устанавливаем наш пакет
-yum localinstall -y rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm
-
+yum localinstall -y /root/rpmbuild/RPMS/x86_64/nginx-1.20.0-1.el7.ngx.x86_64.rpm
 # Запускаем
 systemctl start nginx
-
 # Проверяем
 systemctl status nginx
-
-#
+# Создадим каталог repo
+rm /usr/share/nginx/html/*
 mkdir /usr/share/nginx/html/repo
-
-#
-cp rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
-
-#
-wget http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-0.1-6.noarch.rpm
-
-#
+# Копируем  собранный RPM
+cp -r /root/rpmbuild/RPMS/x86_64/nginx-1.20.0-1.el7.ngx.x86_64.rpm /usr/share/nginx/html/repo/
+# Скачиваем rpm  Percona-server
+wget http://www.percona.com/downloads/percona-release/redhat/1.0-26/percona-release-1.0-26.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-1.0-26.noarch.rpm
+# Инициализируем репозиторий
 createrepo /usr/share/nginx/html/repo/
-
-#
-nano /etc/nginx/conf.d/default.conf
-
-#
+createrepo --update /usr/share/nginx/html/repo/
+# Замена default.conf
+sudo cp -f /vagrant/default.conf /etc/nginx/conf.d/default.conf
+# Проверяем синтаксис
 nginx -t
-
-#
+# перезапускаем NGINX
 nginx -s reload
-
-#
-lynx http://localhost/repo/
-
-#
+#курл на страничку
 curl -a http://localhost/repo/
-
-#
+#добавляем репозирторий
 cat >> /etc/yum.repos.d/otus.repo << EOF
 [otus]
 name=otus-linux
@@ -77,17 +48,9 @@ baseurl=http://localhost/repo
 gpgcheck=0
 enabled=1
 EOF
-
 #
 yum repolist enabled | grep otus
-
 #
 yum list | grep otus
-
 #
 yum install percona-release -y
-
-#
-
-
-#
